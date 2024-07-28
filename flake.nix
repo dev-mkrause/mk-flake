4ec7279e7dab21868803f7d05f1e32c96a13ea0e
Marvin Krause <public@mkrause.org>
2024-07-28 17:43:26 +0200

    add sops-nix

--- flake.nix
+++ flake.nix
@@ -3,14 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     stylix.url = "github:danth/stylix";
   };
 
-  outputs = { nixpkgs, stylix, home-manager, ... }:
+  outputs = { nixpkgs, stylix, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -20,6 +27,7 @@
         modules = [./cachy/configuration.nix
                    ./cachy/hardware-configuration.nix
                    stylix.nixosModules.stylix
+                   inputs.sops-nix.nixosModules.sops
                    home-manager.nixosModules.home-manager {
                      home-manager.useGlobalPkgs = true;
                      home-manager.useUserPackages = true;
